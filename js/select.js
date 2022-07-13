53177e83e818eed35d7817d3347def9b1033e26b
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-13 15:17:03 -0500

    Agregando las cosas que faltaron de la clase de ayer 12 07 2022

--- js/select.js
+++ js/select.js
@@ -1,21 +1,19 @@
-const renderSelect = () => {
-    let select = document.querySelector("#categories")
-    
-    let {genres} = data;
+const renderSelect = (data) => {
+    let select = document.querySelector("#categories");
+    let { genres } = data;
+  
     genres.forEach((gender) => {
-        // Creamos el nuevo elemento <option>
-        let option = document.createElement('option')
-        // Agregamos el atributo "value"
-        option.setAttribute('value', gender.id)
-        option.innerText = gender.name
-        select.appendChild(option);
-        console.log(gender.name)
+      // Creamos el nuevo elemento <option>
+      let option = document.createElement("option");
+      // Agregamos el atributo "value"
+      option.setAttribute("value", gender.id);
+      // Agregamos el texto que corresponde
+      option.innerText = gender.name;
+      // Insertamos el <option> generado dentro del elemento <select>
+      select.appendChild(option);
     });
-};
-
-fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
+  };
+  
+  fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
-
-
-
+    .then((data) => renderSelect(data));
