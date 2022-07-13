c51dd0ec6cf2d4d77c6b1c8914884bee42e75006
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-12 21:24:48 -0500

    Hay ya no se

--- js/select.js
+++ js/select.js
@@ -1,10 +1,21 @@
 const renderSelect = () => {
-    console.log(genres)
-}
+    let select = document.querySelector("#categories")
+    
+    let {genres} = data;
+    genres.forEach((gender) => {
+        // Creamos el nuevo elemento <option>
+        let option = document.createElement('option')
+        // Agregamos el atributo "value"
+        option.setAttribute('value', gender.id)
+        option.innerText = gender.name
+        select.appendChild(option);
+        console.log(gender.name)
+    });
+};
 
 fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
 
 
