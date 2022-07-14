52817a9b26dce228010cabc8f0b43758203a6779
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-14 18:27:39 -0500

    Se agrega la primer parte para mirar las imagenes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
 const API_KEY = `fa900030b5382f71a0cbc261e0a0490f`;
 const URL = `https://api.themoviedb.org/3`;
+const IMG_PREFIX = "https://www.themoviedb.org/t/p/w220_and_h330_face";
--- js/select.js
+++ js/select.js
@@ -1,19 +1,29 @@
 const renderSelect = (data) => {
-    let select = document.querySelector("#categories");
-    let { genres } = data;
+  let select = document.querySelector("#categories");
+  let { genres } = data;
+
+  genres.forEach((gender) => {
+    // Creamos el nuevo elemento <option>
+    let option = document.createElement("option");
+    // Agregamos el atributo "value"
+    option.setAttribute("value", gender.id);
+    // Agregamos el texto que corresponde
+    option.innerText = gender.name;
+    // Insertamos el <option> generado dentro del elemento <select>
+    select.appendChild(option);
+  });
+};
+
+const renderMovies = (data) => {
   
-    genres.forEach((gender) => {
-      // Creamos el nuevo elemento <option>
-      let option = document.createElement("option");
-      // Agregamos el atributo "value"
-      option.setAttribute("value", gender.id);
-      // Agregamos el texto que corresponde
-      option.innerText = gender.name;
-      // Insertamos el <option> generado dentro del elemento <select>
-      select.appendChild(option);
-    });
-  };
-  
-  fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
+}
+
+fetch(`${URL}/genre/movie/list?api_key=${API_KEY}`)
+  .then((response) => response.json())
+  .then((data) => renderSelect(data));
+
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
     .then((response) => response.json())
-    .then((data) => renderSelect(data));
+    .then((data) => {
+      renderMovies(data);
+    });
