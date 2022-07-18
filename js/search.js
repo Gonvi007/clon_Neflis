1327cf707f3e7f1eedef912f352fe67ac4fddb19
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-17 22:25:36 -0500

    Se agrego el codigo para el search

--- js/search.js
+++ js/search.js
@@ -1,3 +1,10 @@
+let busqueda = document.querySelector("#search").ariaValueMax;
+
 movies_array.filter(function(movie){
-    return movie.title.includes("Dog");
+    let titulo = movie.title.toLoweCase();
+    let b = busqueda.toLowerCase();
+    return titulo.includes(b);
+
+    // Esto es lo mismo que lo anterior, pero menos codigo.
+    // return movie.title.toLoweCase().includes(toLowerCase().busqueda);
 });
