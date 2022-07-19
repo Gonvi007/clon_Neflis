cf96ac6941317acaee18c32ad8ea0bbcbc9c0f23
Alexis Gonzalez <alesisgonzalez50@gmail.com>
2022-07-19 16:38:12 -0500

    Se aprendio el search :3

--- js/search.js
+++ js/search.js
@@ -1,13 +1,47 @@
-let busqueda = document.querySelector("#search").value;
+let frmBuscar = document.querySelector("#search");
+// https://api.themoviedb.org/3/search/multi?api_key=fa900030b5382f71a0cbc261e0a0490fquery=100
 
-movies_array.filter(function(movie){
-    let titulo = movie.title.toLoweCase();
-    let b = busqueda.toLowerCase();
-    return titulo.includes(b);
+
+
+const renderResults = () => {
+    console.log(results)
+
+}
+
+frmBuscar.addEventListener("submit", (event) => {
+    event.preventDefault(); //Evita que se recargue y borre la información del boton.
+
+    const {movieName} = event.target
+    renderResults(movieName.value);
+            
+    fetch(`${URL}/search/multi?api_key=${API_KEY}&query=${movieName.value}`)
+        .then((response) => response.json())
+        .then((data) => console.log(data.results));
+});
+
+
+
+    
+        /*
+        // Una busqueda que permite mayusculas y minusculas usando el FILTER
+
+        let peticion = ['100 años de soledad', 'Spiderman', 'Thor', 'Mi villano favorito', 'Dr. Strange'];
+
+        let result = peticion.filter(name => name.toLocaleLowerCase().includes(movieName.value.toLocaleLowerCase()));
+
+        console.log(result);
+        */
+
+
+
+// movies_array.filter(function(movie){
+//     let titulo = movie.title.toLoweCase();
+//     let b = busqueda.toLowerCase();
+//     return titulo.includes(b);
 
     // Esto es lo mismo que lo anterior, pero menos codigo.
     // return movie.title.toLoweCase().includes(toLowerCase().busqueda);
-});
+// });
 /*
 // para busqueda
 nombres = ["Ana", "Benito", "Carlos", "Darius", "Ernesto"];
